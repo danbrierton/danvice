a2fa40c183791ca8378ef56f05258a86b86edaf7
Daniel Brierton <dan.brierton@aol.com>
2019-03-25 09:21:08 -0400

    added files missing from last commit

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+   return(
+      <div id="header_bigDiv">
+         <h1 id="header_name">Name</h1>
+         <h3 id="header_phrase">Phrase</h3>
+      </div>
+   )
+}
+
+export default Header
