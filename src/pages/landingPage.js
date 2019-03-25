56c70db641a52f8a25d1e11820a01ae26d1e26e5
Daniel Brierton <dan.brierton@aol.com>
2019-03-25 09:09:05 -0400

    initial commit, bare bones page

---
+++ src/pages/landingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class LandingPage extends React.Component{
+
+
+   render(){
+      return(
+         <div>
+            <p>This is the Landing Page</p>
+         </div>
+      )
+   }
+}
+
+export default LandingPage
