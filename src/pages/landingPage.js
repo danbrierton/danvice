996095d38460c09f0f44251935207bc0cef55b41
Daniel Brierton <dan.brierton@aol.com>
2019-03-25 09:15:17 -0400

    intial commit, bare bones component for header

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Header from '../components/header'
 
 class LandingPage extends React.Component{
 
@@ -6,6 +7,7 @@ class LandingPage extends React.Component{
    render(){
       return(
          <div>
+            <Header/>
             <p>This is the Landing Page</p>
          </div>
       )
